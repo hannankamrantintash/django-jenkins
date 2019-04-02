15983146d226b41bb5eac92f877227851825cc0f
Hannan <hannan@Hannans-Official-MacBook.local>
2019-04-02 14:51:44 +0500

    Add test

--- MyApp/tests.py
+++ MyApp/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test.runner import DiscoverRunner
 
-# Create your tests here.
+class NoDbTestRunner(DiscoverRunner):
+  """ A test runner to test without database creation """
+
+  def setup_databases(self, **kwargs):
+    """ Override the database creation defined in parent class """
+    pass
