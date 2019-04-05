ae92532894813f28e0b7a4c28743ceb9123d6875
Hannan <hannan@Hannans-Official-MacBook.local>
2019-04-05 10:39:06 +0500

    test jenkins

--- MyApp/tests.py
+++ MyApp/tests.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-class AccountTests(APITestCase):
+class UserTests(APITestCase):
     def test_create_account(self):
         """
         Create new user.
